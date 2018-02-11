0e6d25a7d13182dd5ea1ffa361f495a4b72dddf0
LAPTOP-K41JB88F\peter <peterkoziman>
2018-02-11 17:36:08 +0100

    no message

---
+++ ProgExercises/3-7 Fibonacci Sequence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3_7_Fibonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
